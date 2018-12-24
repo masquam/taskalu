caea39ad41e4a6c6cb9d2593c715626f2d9a70aa
masquam <masquam@DESKTOP..example.com>
2018-12-24 17:58:41 +0900

    TryParseExact

--- taskalu/DateToColorConverter.cs
+++ taskalu/DateToColorConverter.cs
@@ -18,29 +18,43 @@ namespace Taskalu
         {
             string datestring = (string)value;
 
-            DateTime date = DateTime.ParseExact(datestring, "yyyy-MM-dd HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+            DateTime date;
+            DateTime.TryParseExact(datestring,
+                "yyyy-MM-dd HH:mm:ss",
+                System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None,
+                out date);
 
             if (DateTime.Compare(DateTime.Now, date) > 0)
             {
-                return +1;
+                return "2000-01-01 00:00:00";
             }
             else if (DateTime.Compare(DateTime.Today, date.Date) == 0)
             {
-                return -1;
+                return "2000-01-02 00:00:00";
             }
             else
             {
-                return 0;
+                return "2000-01-03 00:00:00";
             }
-
         }
 
         public object ConvertBack(
          object value, Type targetType,
          object parameter, System.Globalization.CultureInfo culture)
         {
-            throw new NotSupportedException("ConvertBack not supported");
+            //throw new NotSupportedException("ConvertBack not supported");
+            return value;
         }
     }
 }
 
+/*
+
+                                                    <DataTrigger Binding="{Binding DueDate, Converter={StaticResource DateToColorConverter}}" Value="2000-01-01 00:00:00">
+                                                        <Setter Property="Foreground" Value="Red" />
+                                                    </DataTrigger>
+                                                    <DataTrigger Binding="{Binding DueDate, Converter={StaticResource DateToColorConverter}}" Value="2000-01-02 00:00:00">
+                                                        <Setter Property="Foreground" Value="Blue" />
+                                                    </DataTrigger>
+ */
