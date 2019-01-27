9102f5146c3e29662195902eff3f5f61e8192b4f
masquam <masquam@DESKTOP..example.com>
2019-01-27 14:24:00 +0900

    createtable test

--- taskaluTests/SQLiteClassTests.cs
+++ taskaluTests/SQLiteClassTests.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
+using System.Data.SQLite;
 
 namespace Taskalu.Tests
 {
@@ -46,8 +47,7 @@ namespace Taskalu.Tests
                 // preperation fail
                 Assert.Fail();
             }
-            FileStream fs = File.Create(path);
-            fs.Close();
+            SQLiteConnection.CreateFile(path);
 
             var result = SQLiteClass.TouchDB(dir, path);
             Debug.Assert(result == false);
@@ -86,11 +86,28 @@ namespace Taskalu.Tests
                 // preperation fail
                 Assert.Fail();
             }
-            FileStream fs = File.Create(path);
-            fs.Close();
+            SQLiteConnection.CreateFile(path);
 
             Debug.Assert(SQLiteClass.CheckTable(path, "tasklist") == false);
         }
 
+        [TestMethod()]
+        public void ExecuteCreateTableTest()
+        {
+            string dir = Path.GetTempPath();
+            string path = dir + "\\taskaludb.sqlite";
+            try
+            {
+                File.Delete(path);
+            }
+            catch (Exception)
+            {
+                // preperation fail
+                Assert.Fail();
+            }
+            SQLiteConnection.CreateFile(path);
+
+            Debug.Assert(SQLiteClass.ExecuteCreateTable(path, "CREATE TABLE tasklist (id INTEGER NOT NULL PRIMARY KEY, name TEXT, description TEXT, memo TEXT, priority TEXT, createdate DATETIME, duedate DATETIME, status TEXT, workholder TEXT)"));
+        }
     }
 }
